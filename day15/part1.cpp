04035f69d4b0f12c564e0458c1cfcdd42f2d01d3
brunoNevess <71086175+brunoNevess@users.noreply.github.com>
2021-12-15 09:23:19 +0000

    Day15

--- day15/part1.cpp
+++ day15/part1.cpp
@@ -1,3 +1,15 @@
+// if the problem only allowed left,down movements , it could be solved by the following: 
+// vector<vector<int>> dp(N,vector<int>(M,INT_MAX));
+// dp[0][0] = 0;
+// for(int i = 1 ; i < M;i++) dp[0][i] = g[0][i] + dp[0][i-1];
+// for(int i = 1 ; i < N;i++) dp[i][0] = g[i][0] + dp[i-1][0];
+// for(int i = 1; i < N ; i++){
+//     for(int j = 1;j<M;j++){
+//         dp[i][j] = g[i][j] +  min(dp[i-1][j],dp[i][j-1]);
+//     }
+// }
+// cout << dp[N-1][M-1] << endl;
+
 #include <bits/stdc++.h>
 #include <climits>
 using namespace std;
